820d0b4a479e7f2dbec80fea6be10b7a9d1f8073
Alex Tucker <tucker.al@gmail.com>
2012-03-11 09:10:39 -0400

    Skeleton classes

---
+++ StringCalculatorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'StringCalculator.php';
+
+class StringCalculatorTest extends PHPUnit_Framework_TestCase {
+
+   public function setUp() {
+
+   }
+
+   public function teatDown() {
+
+   }
+
+   public function testEmptyString() {
+      $this->fail('test');
+   }
+}
